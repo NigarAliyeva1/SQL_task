eec93a75d1901035df5ec785201ecbb18fcd824f
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-03-04 01:30:18 +0400

    Create Chocolate.sql

---
+++ MSSQL Tasks/Chocolate.sql
@@ -0,0 +1,5 @@
+--I love chocolate and only want delicious baked goods that have chocolate in them!
+
+--Write a Query to return bakery items that contain the word "Chocolate".
+SELECT * FROM bakery_items
+Where product_name like '%Chocolate%'
