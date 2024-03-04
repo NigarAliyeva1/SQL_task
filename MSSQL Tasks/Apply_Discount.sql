921712dc98080d22c1a762e7613037eb719852de
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-03-04 21:12:22 +0400

    Create Apply_Discount.sql

---
+++ MSSQL Tasks/Apply_Discount.sql
@@ -0,0 +1,8 @@
+--A Computer store is offering a 25% discount for all new customers over the age of 65 or customers that spend more than $200 on their first purchase.
+
+--The owner wants to know how many customers received that discount since they started the promotion.
+
+--Write a query to see how many customers received that discount.
+
+Select count(customer_id) FROM customers
+WHERE age>65 or total_purchase>200
