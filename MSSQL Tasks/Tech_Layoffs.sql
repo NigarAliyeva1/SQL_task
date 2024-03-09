03d1306e1e2b0ad0a382d368aad6a1df12eda0c6
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-03-09 21:24:01 +0400

    Create Tech_Layoffs.sql

---
+++ MSSQL Tasks/Tech_Layoffs.sql
@@ -0,0 +1,9 @@
+--Tech companies have been laying off employees after a large surge of hires in the past few years.
+
+--Write a query to determine the percentage of employees that were laid off from each company.
+
+--Output should include the company and the percentage (to 2 decimal places) of laid off employees.
+
+--Order by company name alphabetically.
+SELECT company, ROUND(cast(employees_fired as Decimal)/ company_size * 100,2) FROM tech_layoffs
+  order by company
